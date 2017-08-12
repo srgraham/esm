0d012f9c6b16e04fbe7cd30a05d72bd6092318d2
Scott Graham <scottgrahamnc@gmail.com>
2017-08-12 19:23:28 -0400

    :D

--- fields.go
+++ fields.go
@@ -16,7 +16,46 @@ type thing2 struct {
 
 func init() {
 
-	var zero_zstring zstring
+	//var uint8Zero uint8
+	//var uint16Zero uint16
+	//var uint32Zero uint32
+	var uint64Zero uint64
+	//var int8Zero int8
+	//var int16Zero int16
+	//var int32Zero int32
+	//var int64Zero int64
+	//var float32Zero float32
+	//var float64Zero float64
+
+	//var nullZero null
+	//var charZero char
+	//var char4Zero char4
+	//var wcharZero wchar
+	//
+	//var vsvalZero vsval
+	//
+	//var formidZero formid
+	//var irefZero iref
+	//var hashZero hash
+	//var filetimeZero filetime
+	//var systemtimeZero systemtime
+	//var rgbZero rgb
+	//
+	//var lstringZero lstring
+	//var dlstringZero dlstring
+	//var ilstringZero ilstring
+	//var bstringZero bstring
+	//var bzstringZero bzstring
+	//var wstringZero wstring
+	//var wzstringZero wzstring
+	var zstringZero zstring
+
+
+
+
+
+
+
 
 	FieldsStructLookup = make(map[string]map[string]interface{})
 	//FieldsStructLookup = map[string]interface{} {
@@ -27,8 +66,10 @@ func init() {
 		NextObjectId uint32
 	}{}
 
-	FieldsStructLookup["TES4"]["CNAM"] = zero_zstring
-	FieldsStructLookup["TES4"]["MAST"] = zero_zstring
+	FieldsStructLookup["TES4"]["CNAM"] = zstringZero
+	FieldsStructLookup["TES4"]["SNAM"] = zstringZero
+	FieldsStructLookup["TES4"]["MAST"] = zstringZero
+	FieldsStructLookup["TES4"]["DATA"] = uint64Zero
 
 	_ = 9
 }
